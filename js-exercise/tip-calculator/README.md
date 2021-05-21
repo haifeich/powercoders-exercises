661f7e2b32550269314539717cdffefde098feb7
Haifei <haifei.zhang@powercoders.org>
2021-05-21 13:51:48 +0200

    add USD EUR currency feature

--- js-exercise/tip-calculator/README.md
+++ js-exercise/tip-calculator/README.md
@@ -2,5 +2,9 @@ Tip calculator
   
 1. add input for bill ammount and tip rate
 2. calculate the total bill and show the tip ammount. 
+3. Use this free currency API.
+    add 2 additional buttons to HTML: convert to USD / convert to EUR
+    add event listeners and call the API to convert your CHF values to the chosen currency
+    add the result to the DOM
 
 
--- js-exercise/tip-calculator/tip.js
+++ js-exercise/tip-calculator/tip.js
@@ -1,8 +1,14 @@
 const bill = document.querySelector('#bill');
 const tip = document.querySelector('#tip');
-const btn = document.querySelector('input + button');
-const btnReset = document.querySelector('button + button');
-const result = document.querySelector('p');
+const btn = document.querySelector('#chf');
+const btnReset = document.querySelector('#reset');
+const btnUsd = document.querySelector('#usd');
+const btnEur = document.querySelector('#eur');
+const showResult = document.querySelector('p');
+let usdChf = 0;
+let usdEur = 0;
+let outPut = 0;
+let tipOutPut = 0;
 let wrong1 = document.createElement('p'); //create first wrong message in a p tag for bill
 let wrong2 = document.createElement('p'); //create second wrong message in a p tag for tip
 bill.insertAdjacentElement('afterend', wrong1); //place it just after the bill amount button
@@ -14,10 +20,12 @@ function isValide (x){
         return true;  // validation function need both false and true value. if one is missing, it will return undifined. which is also a boolean false value.
     }
 } 
-btn.addEventListener('click', event => {
+btn.addEventListener('click', calculate);
+
+function calculate(){
     const billValue = bill.value;
     const tipValue = tip.value;
-    result.innerHTML = '';
+    showResult.innerHTML = '';
     wrong1.innerHTML = '';
     wrong2.innerHTML = '';
     if (!isValide(billValue)){    //no while because it will loop all the time, no time to reset the correct value
@@ -27,21 +35,47 @@ btn.addEventListener('click', event => {
         wrong2.innerText = 'Please enter the correct tip';
     };
     if (isValide(billValue) && isValide(tipValue)){
-        let outPut = 0;
-        let tipOutPut = 0;
+
         if (Number(billValue) <= 10){ //Don't pay tips when it's less than 10
             outPut = Number(billValue)
-            result.innerText = `You bill is ${outPut} CHF, no need to pay tips.`;
+            showResult.innerText = `You bill is ${outPut} CHF, no need to pay tips.`;
         } else{
             tipOutPut = Math.round((Number(billValue) * (Number(tipValue)/100)) * 20)/20; //the result will like XXX.XX
             outPut = Number(billValue) + tipOutPut;
-            result.innerText = `You bill is ${outPut} CHF total, of which ${tipOutPut} CHF is the tip.`;
+            showResult.innerText = `You bill is ${outPut} CHF total, of which ${tipOutPut} CHF is the tip.`;
         }       
     }
-});
+}
 btnReset.addEventListener('click', event =>{
-    result.innerHTML = '';
+    showResult.innerHTML = '';
     bill.value = '';
     tip.value = '';
     bill.focus();
+})
+
+btnUsd.addEventListener('click', 
+
+    async function currencies (){
+        let response = await fetch('http://api.currencylayer.com/live?access_key=d531a10cda9f3031176efec85541fc6b&currencies=CHF,EUR&format=1');
+        let data = response.json();
+        data.then(result => {usdChf = result.quotes.USDCHF;})
+            .then(()=>{
+                calculate();
+                outPut = Math.round(outPut/usdChf *20)/20;
+                tipOutPut = Math.round(tipOutPut/usdChf *20)/20;
+                showResult.innerText = `You bill is ${outPut} USD total, of which ${tipOutPut} USD is the tip.`;
+            })   
+    })
+
+btnEur.addEventListener('click', 
+async function currencies (){
+    let response = await fetch('http://api.currencylayer.com/live?access_key=d531a10cda9f3031176efec85541fc6b&currencies=CHF,EUR&format=1');
+    let data = response.json();
+    data.then(result => {usdEur = result.quotes.USDEUR; usdChf = result.quotes.USDCHF;})
+        .then(()=>{
+            calculate();
+            outPut = Math.round(outPut/usdChf/usdEur *20)/20;
+            tipOutPut = Math.round(tipOutPut/usdChf/usdEur *20)/20;
+            showResult.innerText = `You bill is ${outPut} EUR total, of which ${tipOutPut} EUR is the tip.`;
+        })   
 })
